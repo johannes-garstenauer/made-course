9476c62634f0755d9befe81e7acd18f8c36fb34f
Johannes Garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2024-11-14 14:07:23 +0100

    Beautify pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -3,14 +3,14 @@
 #######README#############
 
 # Potentially you will have to make this script executable first
-# Use 'chmod +x path_to_repo/project/pipeline.sh
-# Make sure to replace path_to_repo with the path to your repo
+# Use 'chmod +x path_to_repo/project/pipeline.sh'
+# Make sure to replace 'path_to_repo' with the path to your repository.
 
 # Make sure to install the following packages to run this script successfully:
-# pandas
-# retry
-# requests
-# numpy
+# - pandas
+# - retry
+# - requests
+# - numpy
 
 # Note: Unfortunately the Data Portal providing the Chilean dataset is currently offline (14.11.2024).
 # As a result the code for this dataset has been removed from execution.
